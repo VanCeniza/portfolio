eeab835affe1661fa77473dcd6d2396db84612b1
VanCeniza <cenizavanbryan22@gmail.com>
2025-06-23 08:13:45 +0800

    portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import galaxyVideo from './videos/galaxy.mp4';
+import blackHole from './videos/blackhole.mp4';
+import dP from './images/me.png';
+import { useEffect } from 'react';
+import { LuComputer } from "react-icons/lu";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
 
 function App() {
+
+  useEffect(() => {
+    document.title="My Portfolio";
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react
-        </a>
+    <div className="container">
+      <video 
+        className="bg-vid" loop autoPlay muted playsInline
+        src={galaxyVideo}>
+      </video>
+      <header>
+        <div className='topleft'>
+          <img src={dP} alt="myface"></img>
+          <h1><span style={{color: '#9400FF'}}>Van</span>Bryan</h1>
+        </div>
+      
+        <ul>
+          <li><a href='#home'>Home</a></li>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#resume'>Resume</a></li>
+          <li><a href='#skills'>Skills</a></li>
+          <li><a href='#projects'>Projects</a></li>
+        </ul>
+
+        <div className="box-icons">
+          <a href="https://web.facebook.com/vanbryan.ceniza">
+            <i className='facebook'><FaFacebook/></i>
+          </a>
+          <a href="https://github.com/VanCeniza">
+            <i className='github'><FaGithub/></i>
+          </a>
+          <a href="https://mail.google.com/mail/u/cenizavanbryan22@gmail.com/#compose">
+            <i className='gmail'><SiGmail/></i>
+          </a>
+        </div>
       </header>
+
+      <video
+        className="bh-vid" loop autoPlay muted playsInline
+        src={blackHole}>
+      </video>
+
+      <section className='person'>
+        <div className='person-info'>
+          <div className='person-title'>
+            <i><LuComputer /></i>
+            Computer Engineer
+          </div>
+
+          <h1>Hi i'm Van Bryan Ceniza Computer Engineer</h1>
+          <p>I'm a computer engineering student in Technological
+            Institute of the Philippines Manila Campus. Feel free
+            to check out my portfolio.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
-
 export default App;
